161963aff24c24930a28708f8e61a76d4f8b58ee
grbrr <damianbecmer@gmail.com>
2023-10-18 23:37:41 +0200

    minor change to python script

--- scripts/bme280_sensor.py
+++ scripts/bme280_sensor.py
@@ -1,12 +1,12 @@
 import bme280
 import smbus2
-from time import sleep
 
 port = 1
 address = 0x76
-bus = smbus2.SMBus(port)
 
+bus = smbus2.SMBus(port)
 bme280.load_calibration_params(bus,address)
+
 
 def read_all():
     bme280_data = bme280.sample(bus,address)
