7edc183d43be5362efe37328dde5e4cccede153f
grbrr <damianbecmer@gmail.com>
2023-10-21 04:28:39 +0200

    write data from pyton to influx

--- scripts/serialtest.py
+++ scripts/serialtest.py
@@ -1,10 +1,32 @@
 import serial
-
-# Python3 code to demonstrate
-# convert dictionary string to dictionary
-# using json.loads()
 import json
 
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+token = os.getenv('token')
+org = "Home"
+url = "http://weatherpi.local:8086"
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+bucket="Weather"
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+   
+""" for value in range(5):
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
+  )
+  write_api.write(bucket=bucket, org="Home", record=point)
+  time.sleep(1) # separate points by 1 second """
 
 ser = serial.Serial(
     port="/dev/ttyS0",  # Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
@@ -39,3 +61,11 @@ while 1:
         data["Humidity"] = round(data["Humidity"] / 1024, 2)
         data["Pressure"] = data["Pressure"] / 100
         print(str(data))
+        record = (
+            Point("BME280")
+            .tag("Location", "Outside")
+            .field("Temperature", data["Temperature"])
+            .field("Humidity", data["Humidity"])
+            .field("Pressure", data["Pressure"])
+        )
+        write_api.write(bucket=bucket, org="Home", record=record)
