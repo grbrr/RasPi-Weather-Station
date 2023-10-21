a16660c7658b9301d5eb2798c3393a86f22ba2c1
grbrr <damianbecmer@gmail.com>
2023-10-21 03:36:58 +0200

    serialtest adjusted to read bme280 data from stm

--- scripts/serialtest.py
+++ scripts/serialtest.py
@@ -15,10 +15,27 @@ ser = serial.Serial(
     timeout=1,
 )
 
-while 1:
-    x = ser.readline()
-    #print(x.decode("utf-8"))
-    
-    data = json.loads(x)
-    print(str(data))
 
+def get_JSON_from_serial():
+    x = ser.readline()
+    try:
+        data = json.loads(x)
+        return data
+    except Exception as e:
+        print("Error while reading serial:", e)
+        return None
+
+
+"""- int32_t for temperature with the units 100 * °C
+- uint32_t for humidity with the units 1024 * % relative humidity
+- uint32_t for pressure
+     If macro "BME280_64BIT_ENABLE" is enabled, which it is by default, the unit is 100 * Pascal
+     If this macro is disabled, Then the unit is in Pascal"""
+
+while 1:
+    data = get_JSON_from_serial()
+    if data is not None:
+        data["Temperature"] = data["Temperature"] / 100
+        data["Humidity"] = round(data["Humidity"] / 1024, 2)
+        data["Pressure"] = data["Pressure"] / 100
+        print(str(data))
